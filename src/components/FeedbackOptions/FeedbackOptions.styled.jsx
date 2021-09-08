439adc661fec492fbe59724bca70a2c5453df0cb
MarianMoskal <mmm198707@yahoo.com>
2021-09-08 11:18:51 +0300

    Done

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -4,11 +4,12 @@ export const Button = styled.button`
   width: 80px;
   padding: 8px;
   border: none;
+  border-radius: 50%;
   cursor: pointer;
   font-weight: 500;
   color: blue;
   background-color: yellow;
-  &:not(:last-child) {
+  &:not(:last-of-type) {
     margin-right: 20px;
   }
 `;
